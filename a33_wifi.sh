3cff205da46f7e0ffdf02cecc45dcc54eaa5e21d
alex <alex@alex-Latitude-E5440.(none)>
2015-12-31 17:59:45 +0800

    ssv wifi tools

---
+++ a33_wifi.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+load=`getprop sys.wifi.on`
+if [ "$load" == 1 ];then
+	echo "loading ssv6051" > /dev/kmsg
+    insmod /system/vendor/modules/ssv6051.ko
+	sleep 2;
+	busybox ifconfig wlan0 up
+else
+	echo "unloading ssv6051" > /dev/kmsg
+	busybox ifconfig wlan0 down
+	sleep 1;
+	rmmod ssv6051
+fi
+
+
